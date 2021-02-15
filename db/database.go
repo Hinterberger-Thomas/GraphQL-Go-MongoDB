a930bb5a4696156d6a90fa7159e399bf5664717f
Salomon-Novachrono <[Salomon.Novachrono@gmail.com]>
2021-02-15 18:54:54 +0100

    added some stuff

--- db/database.go
+++ db/database.go
@@ -9,7 +9,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func startCon() {
+func startCon(dbConn *mongo.Database) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
@@ -20,6 +20,7 @@ func startCon() {
 		}
 	}()
 	panNoDbFound(client)
+	dbConn = client.Database("GraphQL-test")
 }
 
 func panNoDbFound(client *mongo.Client) {
