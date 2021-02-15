9b1386ac7a695c5cacb1cd5e92fce62fb4ff7da0
Salomon-Novachrono <[Salomon.Novachrono@gmail.com]>
2021-02-15 16:08:20 +0100

    added database

---
+++ db/database.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func startCon() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	panNoDbFound(client)
+}
+
+func panNoDbFound(client *mongo.Client) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err := client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		panic(err)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ go 1.15
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/vektah/gqlparser/v2 v2.1.0
+	go.mongodb.org/mongo-driver v1.4.6 
 )
 
 module github.com/Salomon-Novachrono/graphQL-test
